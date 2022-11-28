019691a685d2ad86faa67e362cfc00311fb1da13
Jeremiah <tilse@pdx.edu>
2022-11-27 21:07:49 -0800

    Merge branch 'main' of github.com:CS465P-565-Fall-2022-Meal-App/meal_app into search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 /** @format */
 
-import React from 'react';
-
 import './App.css';
+import Header from './MyComponents/Header';
+import Body from './MyComponents/Body';
+import Footer from './MyComponents/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './MyComponents/About';
+import Contactus from './MyComponents/Contactus';
 
 import MealApp from './MealApp/MealApp';
 
 export default function App() {
   return (
-    <div className="App">
-      <MealApp />
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<Contactus />} />
+          <Route path="/" element={<Body />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
---
+++ src/MyComponents/Body.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide carousel-fade"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleCaptions"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="1.jpg"
+              className="d-block w-100"
+              alt="Pancake breakfast Picture"
+            />
+            <div className="carousel-caption d-none d-md-block bg-dark textopaque">
+              <h2>Welcome to My Chef</h2>
+              <p>A heaven for Foodies</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="2.jpg"
+              className="d-block w-100"
+              alt="Eggs breakfast Picture"
+            />
+            <div className="carousel-caption d-none d-md-block bg-dark textopaque">
+              <h2>Welcome to My Chef</h2>
+              <p>Give us ingrident we give you Ideas</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="3.jpg"
+              className="d-block w-100"
+              alt="Desert sweetdish picture"
+            />
+            <div className="carousel-caption d-none d-md-block bg-dark textopaque">
+              <h2>Welcome to My Chef</h2>
+              <p>No one is born a great cook, one learns by doing</p>
+            </div>
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+
+      <div className="meal-wrapper">
+        <div className="meal-search">
+          <h2 className="title">My Chef App</h2>
+          <blockquote>
+            Real food doesn't have ingredients, real food is ingredients.
+            <br />
+            <cite>-Jamie Oliver</cite>
+          </blockquote>
+
+          <div className="meal-search-box">
+            <label for="search-input"></label>
+            <input
+              type="text"
+              className="search-control"
+              placeholder="Enter an ingredient"
+              id="search-input"
+            />
+
+            <button type="submit" className="search-btn" id="search-btn">
+              <i className="fas fa-search" aria-hidden="true"></i>GO
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
